66f7abff444e742a61c3b17610723d5049131efc
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2023-02-10 00:06:11 +0300

    initial commit

---
+++ app/src/main/java/com/example/coroutinestart/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.coroutinestart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.example.coroutinestart.databinding.ActivityMainBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            lifecycleScope.launch{
+                loadData()
+            }
+        }
+    }
+
+    private suspend fun loadData() {
+        Log.d("MainActivity" , "Load data start $this")
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemperature(city)
+        binding.tvTemperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+        Log.d("MainActivity" , "Load data finished $this")
+    }
+
+    private suspend fun loadCity(): String {
+        delay(5000)
+        return "Moscow"
+    }
+
+    private suspend fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        delay(5000)
+        return 17
+    }
+}
